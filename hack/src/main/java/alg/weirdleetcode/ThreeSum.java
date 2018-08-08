d6145749a339d7b691772f33cc265117167ba44d
chasse2 <eenglish43@yahoo.com>
2018-08-08 18:18:15 -0400

    Mods

--- hack/src/main/java/alg/weirdleetcode/ThreeSum.java
+++ hack/src/main/java/alg/weirdleetcode/ThreeSum.java
@@ -23,8 +23,50 @@ import java.util.*;
  * Beats only 4.34%! :-(                            ========== MAY REVISIT =========
  * Time Complexity is O(n-squared)
  * Space Complexity is O(n)
+ *
+ *  Given array nums = [-1, 0, 1, 2, -1, -4],
+ *
+ *  Best algorithm (not best - best seems to involve some kind of binary search):
+ *  [
+ *     [-1, 0, 1],
+ *     [-1, -1, 2]
+ *   ]
  */
 public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            // if (nums[i] > 0) break;
+            twosum(nums, i+1, -nums[i], result);
+        }
+        return result;
+    }
+
+    private void twosum(int[] nums, int start, int target, List<List<Integer>> result) {
+        int lo = start;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            if (nums[lo] + nums[hi] == target) {
+                result.add(Arrays.asList(-target, nums[lo], nums[hi]));
+                lo++;
+                hi--;
+                while (lo < hi && nums[lo] == nums[lo-1]) {
+                    lo++;
+                }
+                while (lo < hi && nums[hi] == nums[hi+1]) {
+                    hi--;
+                }
+            } else if (nums[lo] + nums[hi] > target) {
+                hi--;
+            } else {
+                lo++;
+            }
+        }
+    }
+
+    /*
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
 
@@ -50,4 +92,5 @@ public class ThreeSum {
 
         return results;
     }
+    */
 }
--- hack/src/test/java/javalang/FooTest.java
+++ hack/src/test/java/javalang/FooTest.java
@@ -3,6 +3,7 @@ package javalang;
 import javalang.Def;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
@@ -31,46 +32,108 @@ public class FooTest {
 
     @Test
     public final void test() throws Exception {
-        List<String> list = Collections.synchronizedList(new ArrayList<>());
-        synchronized (list) {
-            if (!list.isEmpty()) {
-                list.remove(0);
-            }
+//        final String line = "81 : (1,53.38,$45) (2,88.62,$98) (3,78.48,$3) (4,72.30,$76) (5,30.18,$9) (6,46.34,$48)";
+        final String line = "75 : (1,85.31,$29) (2,14.55,$74) (3,3.98,$16) (4,26.24,$55) (5,63.69,$52) (6,76.25,$75) (7,60.02,$74) (8,93.18,$35) (9,89.95,$78)";
+        final String[] parts = line.split(" : ");
+        final String[] items = parts[1].split(" ");
+
+        final int W = Integer.valueOf(parts[0]) * 100;
+        final int n = items.length;
+
+        final int[] v = new int[n];
+        final int[] w = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            final String s = items[i].substring(1, items[i].length() - 1);
+            final String[] item = s.split(",");
+            final int num = Integer.valueOf(item[0]);
+            final int weight = new BigDecimal(item[1]).scaleByPowerOfTen(2).intValue();
+            final int cost = Integer.valueOf(item[2].substring(1));
+            w[i] = weight;
+            v[i] = cost;
         }
+
+        int x = 0;
+        solve3(v, w, n, W);
     }
 
-    public int searchInsert(int[] nums, int target) {
-        if (nums.length <= 0) {
-            return 0;
+    private int[] solve3(final int[] v, final int[] w, final int n, final int W) {
+        int[][] m = new int[n + 1][W + 1];
+
+        for (int j = 0; j <= W; ++j) {
+            m[0][j] = 0;
         }
 
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= W; ++j) {
+//              if (w[i] > j) {
+                if (w[i-1] > j) {
+                    m[i][j] = m[i - 1][j];
+                    if (m[i][j] != 0) {
+                        System.out.println("a " + i + " " + j + " " + m[i][j]);
+                    }
+                } else {
+//                    m[i][j] = Math.max(m[i-1][j], m[i-1][j-w[i]] + v[i]);
+                    m[i][j] = Math.max(m[i-1][j], m[i-1][j-w[i-1]] + v[i-1]);
+                    if (m[i][j] != 0) {
+                        System.out.println("b " + i + " " + j + " " + m[i][j]);
+                    }
+                }
             }
+        }
 
-            if (target < nums[mid]) {
-                if (mid == 0) {
-                    return 0;
-                }
-                if (target > nums[mid - 1]) {
-                    return mid;
-                }
+        return null;
+        /*
+        // Input:
+// Values (stored in array v)
+// Weights (stored in array w)
+// Number of distinct items (n)
+// Knapsack capacity (W)
+// NOTE: The array "v" and array "w" are assumed to store all relevant values starting at index 1.
 
-                high = mid - 1;
+for j from 0 to W do:
+    m[0, j] := 0
+
+for i from 1 to n do:
+    for j from 0 to W do:
+        if w[i] > j then:
+            m[i, j] := m[i-1, j]
+        else:
+            m[i, j] := max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+         */
+    }
+    private int solve2(final int[] arr) {
+        final Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; ++i) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
             } else {
-                if (mid == nums.length - 1) {
-                    return nums.length;
-                }
-                low = mid + 1;
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                return entry.getKey();
             }
         }
 
         return -1;
     }
+
+    private int solve(final int n) {
+        int result = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                continue;
+            }
+
+            result += i;
+        }
+
+        return result;
+    }
 }
+
